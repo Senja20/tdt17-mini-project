1433746207a7c0d9e7b1d9027ef7c8f3e483e52e
Yauhen Yavorski <yauheny@stud.ntnu.no>
2024-11-14 17:22:27 +0100

    ✨ Update main script to load datasets from environment variables and implement DataLoader for training and validation

--- __main__.py
+++ __main__.py
@@ -1,12 +1,27 @@
-from modal import PoleDataset
+
+print('Hello, World!')
+from model import PoleDataset
+from dotenv import load_dotenv
+from os import getenv
+from torch.utils.data import DataLoader
+from utils import collate_fn, get_device
+
+load_dotenv()
 
 if __name__ == '__main__':
-    dataset = PoleDataset(
-        '/cluster/projects/vc/data/ad/open/Poles/train/images',
-        '/cluster/projects/vc/data/ad/open/Poles/train/labels',
+    device = get_device()
+
+    path_to_train_images: str = getenv('PATH_TO_TRAIN_DATA')
+    path_to_val_images: str = getenv('PATH_TO_VAL_DATA')
+
+    train_dataset = PoleDataset(
+        path_to_train_images + '/images',
+        path_to_train_images + '/labels',
     )
-    print(len(dataset))
-    sample = dataset[0]
-    print(sample)
-    print(sample.image)
-    print(sample.annotations)
+    val_dataset = PoleDataset(
+        path_to_val_images + '/images',
+        path_to_val_images + '/labels',
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
