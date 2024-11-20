297089594e7301524df1c0eb3cec80545eb66844
Yauhen Yavorski <yauheny@stud.ntnu.no>
2024-11-20 14:03:52 +0100

    ✨  training parameters include hyperparameters for model optimization

--- __main__.py
+++ __main__.py
@@ -8,16 +8,29 @@ model = YOLO("yolov9c.pt")
 
 model.info()
 
+# essential training parameters
 train_params = {
     "data": "./data.yaml",
-    "epochs": 100,
     "imgsz": (1024, 128),  # (width, height)
     "plots": True,  # plot results.txt as results.png
-    "workers": 4,  # workers are used for loading data
+    "workers": 8,  # workers are used for loading data
     "batch": 16,
     "rect": True,  # rectangular training (keep aspect ratio)
     "patience": 50,  # early stopping patience
+    "project": "pole_detection",  # save to project/name
 }
+
+# Hyperparameters
+hyper_params = {
+    "epochs": 100,
+    "name": "model_optimization",
+    "optimizer": "AdamW",  # Optimize for mobile
+    "lr0": 0.01,
+    "lrf": 0.0001,
+    "momentum": 0.937,
+}
+
+train_params = {**train_params, **hyper_params}
 
 results = model.train(**train_params)
 
